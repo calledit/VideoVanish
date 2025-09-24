4def2e3aadc33a02e0a39e8af05a6bd78d53878c
calledit <1573053+calledit@users.noreply.github.com>
2025-09-24 13:56:58 +0200

    Create windows_install.bat

---
+++ windows_install.bat
@@ -0,0 +1,21 @@
+echo windows installer for VideoVanish
+
+set "CONDA=%UserProfile%\miniconda3\condabin\conda.bat"
+CALL "%CONDA%"  create -n videovanish python=3.11 -y
+CALL "%UserProfile%\miniconda3\Scripts\activate.bat" videovanish
+
+
+
+CALL "%CONDA%" install pytorch torchvision pytorch-cuda=12.4 -c pytorch -c nvidia -y
+
+git clone https://github.com/calledit/sam2_numpy_frames
+cd sam2_numpy_frames
+pip install -e .
+cd checkpoints
+curl -O https://dl.fbaipublicfiles.com/segment_anything_2/092824/sam2.1_hiera_large.pt
+cd ..
+
+git clone https://github.com/calledit/DiffuEraser_np_array
+pip install einops diffusers==0.29.2 transformers scipy matplotlib accelerate peft
+
+rem do i need this? KMP_DUPLICATE_LIB_OK=TRUE
