85ec460c369c4d60ed379b514cb360c0e98ea4a0
calledit <1573053+calledit@users.noreply.github.com>
2025-09-29 15:35:07 +0200

    Add info about waiting for GUI on first execution

--- start_videovanish.bat
+++ start_videovanish.bat
@@ -3,7 +3,7 @@ echo windows installer for VideoVanish
 set "CONDA=%UserProfile%\miniconda3\condabin\conda.bat"
 CALL "%UserProfile%\miniconda3\Scripts\activate.bat" videovanish
 
-
+echo Starting videovanish... Please wait.. It may take a few minutes before the GUI becomes visible the first run as packages need to be compiled the first time
 set KMP_DUPLICATE_LIB_OK=TRUE
 python videovanish.py 
 pause
