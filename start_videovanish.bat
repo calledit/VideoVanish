f76a6d4364f6ea5451e5e4b34dddd75d4ad94601
calledit <1573053+calledit@users.noreply.github.com>
2025-10-22 15:25:42 +0200

    Change echo message for VideoVanish startup

--- start_videovanish.bat
+++ start_videovanish.bat
@@ -1,4 +1,4 @@
-echo windows installer for VideoVanish
+echo windows startup of VideoVanish
 
 set "CONDA=%UserProfile%\miniconda3\condabin\conda.bat"
 CALL "%UserProfile%\miniconda3\Scripts\activate.bat" videovanish
